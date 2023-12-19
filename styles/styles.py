d311c71916b71ecb86c7e1abe06d17af746070fb
mikeredev <mikeredev@gmail.com>
2023-12-19 04:26:43 +0000

    links

--- styles/styles.py
+++
@@ -1,9 +0,0 @@
-OK          = "#ffffff"     # normal operations
-WARN        = "#ffa500"     # take care
-NOK         = "#ff0000"     # alert!
-ACTIVE      = "#a9a9a9"     # device on/high
-INACTIVE    = "#999999"     # device on/low
-NONE        = "#666666"     # device off / device not found
-
-FONT        = "Inconsolata Bold 11"
-GLYPHS      = "Font Awesome 6 Free Regular 11"
